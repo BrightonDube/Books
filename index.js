9bf4bbb29528215919caceee1a9307b7d53755ed
Brighton <BrightonDube@users.noreply.github.com>
2025-03-13 16:12:55 +0200

    `Added GET routes for '/' and '/contact' and '/about' endpoints`

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome home!</h1>");
+});
+app.get('/contact', (req, res) => {
+    res.send("<h1>Contact me</h1><p>You can call me on +270730639639</p>");
+});
+app.get('/about', (req, res) => {
+    res.send("<h1>About</h1> <p>I'm Brighton Dube. A restaurant manager looking to make a career change into software development.</p>");
+});
 app.listen(port, () => {
     console.log(`Server running on ${port}`);
 })
